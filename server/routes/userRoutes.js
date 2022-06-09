22789c07b9afb023a90ebe1d8c8c7854db1be553
Xaniar Vakili <joker427861@gmail.com>
2022-06-09 17:02:44 +0430

    add user routes

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+
+router.post('/login', loginUser)
+router.post('/signup', signupUser)
+router.get('/:id', getUser)
+
+module.exports = router
