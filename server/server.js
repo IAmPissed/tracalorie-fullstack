2e5a38c6b70241997b333a8e11bb763f656e85c1
Xaniar Vakili <joker427861@gmail.com>
2022-06-09 17:03:45 +0430

    use user and foodItem routes

--- server/server.js
+++ server/server.js
@@ -8,4 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/food-items', require('./routes/foodItemRoutes'))
+
 app.listen(PORT, () => console.log(`Server Up and Running on port ${PORT}...`.bold))
