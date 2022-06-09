8693307664a02047a8e55caec7c20a1cb54a4a8d
Xaniar Vakili <joker427861@gmail.com>
2022-06-09 16:44:33 +0430

    add middleware to accept json and urlencoded values

--- server/server.js
+++ server/server.js
@@ -5,4 +5,7 @@ const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 3000
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.listen(PORT, () => console.log(`Server Up and Running on port ${PORT}...`.bold))
