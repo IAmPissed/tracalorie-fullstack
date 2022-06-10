38e334b8ca01fcd444daf1c9cbd096b99abd5a75
Xaniar Vakili <joker427861@gmail.com>
2022-06-10 08:12:19 +0430

    setup food items routes(public)

--- server/routes/foodItemRoutes.js
+++ server/routes/foodItemRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const { getFoodItems, setFoodItem, updateFoodItem, deleteFoodItem } = require('../controllers/foodItemController')
 
 router.route('/').get(getFoodItems).post(setFoodItem)
 router.route('/:id').delete(deleteFoodItem).put(updateFoodItem)
