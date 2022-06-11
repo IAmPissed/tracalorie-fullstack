d781cdf26c2e058f4a92c426b2d2dc4bbfac0fb6
Xaniar Vakili <joker427861@gmail.com>
2022-06-11 10:32:54 +0430

    connect db and log connection

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
+const { connectDB } = require('./config/db')
 
 const PORT = process.env.PORT || 3000
 const app = express()
+
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
