e82b066d0e9c88aafaac19f2d596117ca232912a
Xaniar Vakili <joker427861@gmail.com>
2022-06-12 15:49:24 +0430

    add public static folder

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const express = require('express')
+const path = require('path')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { connectDB } = require('./config/db')
+const expressLayouts = require('express-ejs-layouts')
 
 const PORT = process.env.PORT || 3000
 const app = express()
-
+const publicDirectoryPath = path.join(__dirname, 'public')
+const stylesDirectoryPath = path.join(publicDirectoryPath, 'styles')
 connectDB()
+
+
+app.use(express.static(stylesDirectoryPath))
+
+app.use(expressLayouts)
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
+
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
