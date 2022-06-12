4afc60da776f89c612c2dad3df6de49fdfe87e1e
Xaniar Vakili <joker427861@gmail.com>
2022-06-12 08:13:32 +0430

    add placeholder methods to user and foodItem models

---
+++ server/models/foodItemModel.js
@@ -0,0 +1,18 @@
+const { pool } = require('../config/db')
+
+class FoodItem {
+    static async findAll(id) {
+
+    }
+    static async create(foodItem) {
+
+    }
+    static async findByIdAndUpdate(id) {
+
+    }
+    static async findByIdAndDelete(id) {
+
+    }
+}
+
+module.exports = FoodItem
---
+++ server/models/userModel.js
@@ -0,0 +1,15 @@
+const { pool } = require('../config/db')
+
+class User {
+    static async findByEmail(email) {
+
+    }
+    static async create(user) {
+
+    }
+    static async findById(id) {
+
+    }
+}
+
+module.exports = User
