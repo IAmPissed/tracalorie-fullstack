ae9183e7617095607cd5c4b6ad730496a5ffd670
Xaniar Vakili <joker427861@gmail.com>
2022-06-16 16:22:03 +0430

    setup get all food items for a user

--- server/models/foodItemModel.js
+++ server/models/foodItemModel.js
@@ -2,7 +2,13 @@ const { pool } = require('../config/db')
 
 class FoodItem {
     static async findAll(id) {
-
+        try {
+            const sql = `SELECT * FROM food_item WHERE user_id = $1`
+            const foodItems = (await pool.query(sql, [id])).rows
+            return foodItems
+        } catch (error) {
+            console.log(error.message)
+        }
     }
     static async create(foodItem) {
 
