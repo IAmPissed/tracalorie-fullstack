f38d08b097e8e980863ca38f2b18faa06e6d1896
Xaniar Vakili <joker427861@gmail.com>
2022-06-16 19:04:13 +0430

    return user on create, fix wrong column name

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -12,15 +12,15 @@ class User {
     static async create(user) {
         try {
             const { name, email, password } = user
-            const sql = `INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`
-            await pool.query(sql, [name, email, password])
+            const sql = `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`
+            return (await pool.query(sql, [name, email, password])).rows[0]
         } catch (error) {
             console.log(error.message)
         }
     }
     static async findById(id) {
         try {
-            const sql = `SELECT * FROM users WHERE id = $1`
+            const sql = `SELECT * FROM users WHERE user_id = $1`
             return (await pool.query(sql, [id])).rows[0]
         } catch (error) {
             console.log(error.message)
