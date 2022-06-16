3ebfd82d77e90e1b8f5954d170fff9c6158c3de1
Xaniar Vakili <joker427861@gmail.com>
2022-06-16 11:52:58 +0430

    setup find user by id

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,7 +2,12 @@ const { pool } = require('../config/db')
 
 class User {
     static async findByEmail(email) {
-
+        try {
+            const sql = `SELECT * FROM users WHERE email = $1`
+            return (await pool.query(sql, [email])).rows[0]
+        } catch (error) {
+            console.log(error.message)
+        }
     }
     static async create(user) {
 
