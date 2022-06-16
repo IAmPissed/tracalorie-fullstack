c7fe0a849abfd66d6c0869d1e1ca0b853c02eef3
Xaniar Vakili <joker427861@gmail.com>
2022-06-16 20:14:32 +0430

    make foodItem routes private/protected

--- server/routes/foodItemRoutes.js
+++ server/routes/foodItemRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { getFoodItems, setFoodItem, updateFoodItem, deleteFoodItem } = require('../controllers/foodItemController')
+const { protect } = require('../middlewares/authMiddleware')
 
-router.route('/').get(getFoodItems).post(setFoodItem)
-router.route('/:id').delete(deleteFoodItem).put(updateFoodItem)
+router.route('/').get(protect, getFoodItems).post(protect, setFoodItem)
+router.route('/:id').delete(protect, deleteFoodItem).put(protect, updateFoodItem)
 
 module.exports = router
