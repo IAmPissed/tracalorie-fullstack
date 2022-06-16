061cdda489179ce73124ff39126762b48af86996
Xaniar Vakili <joker427861@gmail.com>
2022-06-16 19:48:04 +0430

    add validation and token genration utils

--- server/utils/userContollerUtils.js
+++ server/utils/userContollerUtils.js
@@ -1,15 +1,37 @@
-const userAlreadyExists = async (email) => {
+const REGEX_PATTERNS = {
+    name: /^([a-zA-Z]{3,40})\s([a-zA-Z]){3,40}$/,
+    email: /^[a-z\d\.]+@([a-z\-]+\.)+[a-z\-]{2,6}/
+}
+
+const userAlreadyExists = async (email, User) => {
     return await User.findByEmail(email)
 }
-const isPasswordShort = (password) => {
-    return password.length < 8
+const isPasswordLengthValid = (password) => {
+    return password.length < 8 || password.length > 20
 }
 const passwordsDoNotMatch = (password, passwordConfirmation) => {
     return password !== passwordConfirmation
 }
+const isMissingField = (request) => {
+    const { name, email, password, passwordConfirmation } = request.body
+    return name === '' || email === '' || password === '' || passwordConfirmation === ''
+}
+const isNameInvalid = (name) => {
+    return REGEX_PATTERNS.name.test(name) === false
+}
+const isEmailInvalid = (email) => {
+    return REGEX_PATTERNS.email.test(email) === false
+}
+const generateJWTToken = (jwt, userId) => {
+    return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '12h' })
+}
 
 module.exports = {
     userAlreadyExists,
-    isPasswordShort,
-    passwordsDoNotMatch
+    isPasswordLengthValid,
+    passwordsDoNotMatch,
+    isMissingField,
+    isNameInvalid,
+    isEmailInvalid,
+    generateJWTToken
 }
